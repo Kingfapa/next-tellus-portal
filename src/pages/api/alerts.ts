72a73f3660132658920e74879d74993d6c2220d3
Adam Maritz <14345584+Kingfapa@users.noreply.github.com>
2024-01-23 14:36:41 +0100

    alert support

---
+++ src/pages/api/alerts.ts
@@ -0,0 +1,33 @@
+import { AxiosError } from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+import { logger } from "../../lib/logger";
+import { readFile } from "fs/promises";
+import { join } from "path";
+import { IAlertProps } from "..";
+
+export interface IAlertsApiResponse {
+  records: IAlertProps[];
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (req.method !== "GET") {
+      throw new Error("Method is not GET...");
+    }
+
+    const data = await readFile(
+      join(process.env.DATA_PATH || "./data", "alerts.json"),
+      { encoding: "utf-8" }
+    ); // ../../data/alerts.json
+
+    res.status(200).json(JSON.parse(data));
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      logger.error((err as AxiosError).response?.data);
+    }
+    res.statusMessage = (err as Error).message;
+    res.status(500).json({ message: (err as Error).message });
+  }
+};
+
+export default handler;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,52 @@
-import { Container } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertStatus,
+  AlertTitle,
+  Container,
+} from "@chakra-ui/react";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import { FormLayout } from "src/components/Form";
 import Header from "src/components/Header";
+import { IAlertsApiResponse } from "./api/alerts";
+
+export interface IAlertProps {
+  alertType: AlertStatus;
+  title: string;
+  description: string;
+}
 
 const Index = () => {
+  const [alerts, setAlerts] = useState<IAlertProps[]>([]);
+
+  useEffect(() => {
+    const getAlerts = async () => {
+      const response = await fetch("/api/alerts");
+      setAlerts(((await response.json()) as IAlertsApiResponse).records);
+    };
+
+    getAlerts();
+  }, []);
   return (
     <>
       <Header />
       <Head>
         <title>Kontaktformulär</title>
       </Head>
+      {/* PageAlerts */}
+      {alerts &&
+        alerts.map((alert, idx) => (
+          <Alert key={idx} status={alert.alertType}>
+            <AlertIcon />
+            {alert.title && <AlertTitle>{alert.title}</AlertTitle>}
+            {alert.description && (
+              <AlertDescription>{alert.description}</AlertDescription>
+            )}
+          </Alert>
+        ))}
+
       <Container
         shadow={"lg"}
         my={{
